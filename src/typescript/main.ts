6cfe9ae4875ce09ce40b74f7c1ff2b5609d6d0d5
somestudentcoder <christopher.oser@student.tugraz.at>
2021-03-11 17:40:12 +0100

    base layer is set

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -2,12 +2,20 @@
 //import { Point } from "./point";
 
 const canvas = <HTMLCanvasElement> document.querySelector('#Canvas');
+const button = <HTMLButtonElement>document.querySelector('#button');
+
 const width = canvas.width;
 const height = canvas.height;
 
 canvas.addEventListener("click", (e:MouseEvent) => newPoint(e.clientX , e.clientY));
+button.addEventListener("click", (e:MouseEvent) => animationID = setInterval(sweepLine, 10));
+
+let clicked = false;
+let animationID: number;
+let current_width = 0;
 
 const ctx = canvas.getContext('2d');
+
 ctx.fillStyle = 'rgb(153, 153, 153)';
 ctx.strokeRect(0, 0, width, height);
 
@@ -15,6 +23,10 @@ var pointList = [];
 
 function newPoint(x, y)
 {
+    if(clicked)
+    {
+        return;
+    }
     x = x - canvas.offsetLeft;
     y = y - canvas.offsetTop;
     ctx.fillStyle = 'rgb(0, 0, 0)';
@@ -29,9 +41,54 @@ function printList()
     console.log(pointList);
 }
 
+function drawPoints()
+{
+    pointList.forEach(point => {
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+        ctx.beginPath();
+        ctx.arc(point.X, point.Y, 3.5, 0, 360, false);
+        ctx.fill();
+    });
+}
+
 function sweepLine()
 {
+    //CHECKS
+    if(!clicked)
+    {
+        button.disabled = true;
+        clicked = true;
 
+        //ALGORITHM INIT
+        var siteEvents = pointList.slice(0);
+        siteEvents.sort(function(a,b){
+            var diff = b.Y - a.Y;
+            if (diff) {return diff;}
+            return b.X - a.X;
+        });
+        console.log(siteEvents)
+    }
+    if(pointList.length == 0)
+    {
+        clearInterval(animationID);
+        clicked = false;
+        button.disabled = false;
+        return;
+    }
+
+    //ALGORITHM
+    
+
+    //CLEAR AND REDRAW
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'rgb(153, 153, 153)';
+    ctx.strokeRect(0, 0, width, height);
+    drawPoints();
+    ctx.strokeStyle = 'rgb(0, 0, 0)';
+    ctx.moveTo(current_width, 0);
+    ctx.lineTo(current_width, canvas.height);
+    ctx.stroke();
+    current_width += 1;
 }
 
 class Point{
